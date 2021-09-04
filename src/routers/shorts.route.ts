cbeec308c701437afde60edc9c422f8f8471e57b
ChiShouWu <u0151033@nkfust.edu.tw>
2021-09-04 23:43:01 +0800

    add cors for frontend localhost:3000

--- src/routers/shorts.route.ts
+++ src/routers/shorts.route.ts
@@ -27,7 +27,7 @@ shortRouter.get('/:hash',
   async (req: Request, res: Response) => {
     try {
       const data = await Shorts.getUrl(req.params.hash);
-      return res.status(302).json(data).send();
+      return res.status(302).json({ url: data!.originUrl }).send();
     } catch (e) {
       const error = e as HttpRequestError;
       return res.status(error.status).send(error.message);
